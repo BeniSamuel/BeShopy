467feaf40cfe009fd94b54f239c81366daf908b3
Beni Samuel <benisameul566@gmail.com>
2026-02-27 18:09:48 +0200

    fix: improved card

--- src/Components/Home/BestSelling/BestDataMapping.jsx
+++ src/Components/Home/BestSelling/BestDataMapping.jsx
@@ -2,19 +2,28 @@ import React from "react";
 import bestSelling from "../../../Data/BestSelling/BestSelling.js";
 import BestCard from "./BestCard.jsx"
 
-const BestDataMapping = () => {
-    console.log(bestSelling);
+const BestDataMapping = ({ isVisible }) => {
   return (
-    <div className=" flex flex-col items-center md:flex-row md:justify-center gap-20">
-      {bestSelling.map((product) => {
+    <div className="flex flex-col items-center md:flex-row md:justify-center gap-8 lg:gap-12">
+      {bestSelling.map((product, index) => {
         return (
-          <BestCard
+          <div
             key={product.id}
-            imgSource={product.imgSource}
-            description={product.description}
-            price={product.price}
-            rating={product.rating}
-          />
+            className={`transition-all duration-700 ${
+              isVisible 
+                ? 'opacity-100 translate-y-0' 
+                : 'opacity-0 translate-y-10'
+            }`}
+            style={{ transitionDelay: `${index * 150}ms` }}
+          >
+            <BestCard
+              id={product.id}
+              imgSource={product.imgSource}
+              description={product.description}
+              price={product.price}
+              rating={product.rating}
+            />
+          </div>
         );
       })}
     </div>
