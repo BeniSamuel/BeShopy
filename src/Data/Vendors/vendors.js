869d9893be6d408d26d6a84369f00e0700dbfac1
Beni Samuel <benisamuel566@gmail.com>
2026-02-28 19:21:11 +0200

    feat: added vendors data

---
+++ src/Data/Vendors/vendors.js
@@ -0,0 +1,23 @@
+const vendors = [
+  {
+    id: "v1",
+    name: "Modern Threads",
+    email: "modern.threads@beshopy.com",
+    brandColor: "#224F34",
+  },
+  {
+    id: "v2",
+    name: "Urban Vogue",
+    email: "urban.vogue@beshopy.com",
+    brandColor: "#6BC785",
+  },
+  {
+    id: "v3",
+    name: "Classic Couture",
+    email: "classic.couture@beshopy.com",
+    brandColor: "#A3F3BE",
+  },
+];
+
+export default vendors;
+
